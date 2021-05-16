beacf35029367b963fa5c4b5adbaacaf96c47f39
Daniel Finke <danielfinke2011@gmail.com>
2021-05-16 09:28:11 -0700

    Wrap app in Redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Application from './components/Application';
+import rootReducer from '../reducers';
 
 import './index.scss';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  document.getElementById('root'),
+);
